d6b093995db27ebd929b19734a6fce4903ad6621
Maykel Felipe Nekel <maykelnekel@gmail.com>
2025-05-05 20:51:45 -0300

    feat: mpv with beneficts looping

--- src/main.py
+++ src/main.py
@@ -48,66 +48,89 @@ def coletar_dados(inputData):
 
         page.wait_for_load_state()
 
-        benefict_name = page.locator("div.responsive > strong").nth(0).inner_text()
-        response = page.locator("div.responsive > table > tbody > tr").nth(0)
-
-        recieved_value = response.locator("td").nth(-1).inner_text().strip()
-        response.locator("td > a").nth(0).click()
-
-        page.wait_for_load_state("load")
-
-        sleep(15)
-
-        page.wait_for_load_state("load")
-
-        if cookie_btn.is_visible():
-            cookie_btn.click()
-
-        screenshot_path = f"page_{datetime.now().isoformat()}.png"
-
-        recieved_incomes_thead = page.locator("th").all()
-
-        sleep(1)
-
-        recieved_incomes_keys = []
-
-        for th in recieved_incomes_thead:
-            recieved_incomes_keys.append(th.inner_text().strip())
-
-        sleep(1)
-
-        recieved_incomes_values = page.locator("tr").all()
         beneficts = []
+        benefict_deatils = []
 
-        for tr in recieved_incomes_values:
-            item = tr.locator("td").all()
-            data = {}
-            for i, td in enumerate(item):
-                data[recieved_incomes_keys[i]] = td.inner_text().strip()
-                beneficts.append(data)
+        all_beneficts = page.locator("div.responsive > table > tbody > tr").count()
 
-        page.screenshot(path=screenshot_path, full_page=True)
+        for benefict_index in range(all_beneficts):
+            benefict_name = (
+                page.locator("div.responsive > strong").nth(benefict_index).inner_text()
+            )
+            response = page.locator("div.responsive > table > tbody > tr").nth(
+                benefict_index
+            )
 
-        with open(screenshot_path, "rb") as img_file:
-            imagem_base64 = base64.b64encode(img_file.read()).decode("utf-8")
+            recieved_value = response.locator("td").nth(-1).inner_text().strip()
+            response.locator("td > a").nth(0).click()
+
+            actual_benefict = {}
+            actual_benefict["nome_do_beneficio"] = benefict_name
+            actual_benefict["valor"] = recieved_value
+
+            page.wait_for_load_state("load")
+
+            sleep(15)
+
+            page.wait_for_load_state("load")
+
+            if cookie_btn.is_visible():
+                cookie_btn.click()
+
+            screenshot_path = f"page_{datetime.now().isoformat()}.png"
+
+            recieved_incomes_thead = page.locator("th").all()
+
+            sleep(1)
+
+            recieved_incomes_keys = []
+
+            for th in recieved_incomes_thead:
+                recieved_incomes_keys.append(th.inner_text().strip())
+
+            sleep(1)
+
+            recieved_incomes_values = page.locator("tr").all()
+
+            for tr in recieved_incomes_values:
+                item = tr.locator("td").all()
+                data = {}
+                for i, td in enumerate(item):
+                    data[recieved_incomes_keys[i]] = td.inner_text().strip()
+                    benefict_deatils.append(data)
+
+            page.screenshot(path=screenshot_path, full_page=True)
+
+            with open(screenshot_path, "rb") as img_file:
+                imagem_base64 = base64.b64encode(img_file.read()).decode("utf-8")
+
+            actual_benefict["detalhes"] = benefict_deatils
+            # actual_benefict["screenshot_base64"] = imagem_base64
+            beneficts.append(actual_benefict)
+
+            os.remove(screenshot_path)
+
+            page.go_back()
+            page.wait_for_load_state("load")
+            sleep(1)
+            if cookie_btn.is_visible():
+                cookie_btn.click()
+            page.locator("button.header").nth(0).click()
 
         results = {
             "termo_da_busca": inputData,
             "data_consulta": datetime.now().isoformat(),
-            "beneficio": benefict_name,
-            "valor": recieved_value,
-            "beneficios": [beneficts],
-            "imagem_base64": imagem_base64,
+            "beneficios": beneficts,
         }
 
         browser.close()
-        os.remove(screenshot_path)
-        print(results)
+
         return results
 
 
+# 1.611.461.617-1
 def main():
-    dados = coletar_dados("maykel felipe")
+    dados = coletar_dados("1.611.461.617-1")
     with open("results.json", "w") as f:
         json.dump(dados, f, indent=2, ensure_ascii=False)
 
