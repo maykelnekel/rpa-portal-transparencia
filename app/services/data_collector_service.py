e49b9495b53b311b7f684e5f6b117a652ad52103
Maykel Felipe Nekel <maykelnekel@gmail.com>
2025-05-08 01:22:48 -0300

    feat: implementing patchright to avoid captcha

--- app/services/data_collector_service.py
+++ app/services/data_collector_service.py
@@ -1,13 +1,12 @@
 import os
 import base64
+import random
+import asyncio
+from datetime import datetime
+from patchright.async_api import async_playwright
 
-from playwright.async_api import async_playwright
 from app.models.person import SearchResults
 from app.config import settings
-from datetime import datetime
-
-# from time import await sleep
-import asyncio
 
 
 async def collect_data_async(input_data: str, filter: bool = False) -> SearchResults:
@@ -19,27 +18,41 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
 
         playwright = await async_playwright().start()
 
-        browser = await playwright.firefox.launch(headless=False)
+        browser = await playwright.chromium.launch(headless=True)
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+        options = await browser.new_context(user_agent=user_agent)
 
-        page = await browser.new_page()
+        page = await options.new_page()
 
         await page.goto(settings.PORTAL_URL)
 
         await page.wait_for_load_state("load")
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         cookie_btn = page.locator("#accept-all-btn")
+
+        await page.mouse.move(random.random() * 800, random.random() * 800)
 
         if await cookie_btn.is_visible():
             await cookie_btn.click()
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         await page.locator("a#link-consulta-pessoa-fisica").click()
+
+        await page.mouse.move(random.random() * 800, random.random() * 800)
 
         await page.wait_for_load_state("load")
 
         if await cookie_btn.is_visible():
             await cookie_btn.click()
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         await asyncio.sleep(1)
+
+        await page.mouse.move(random.random() * 800, random.random() * 800)
 
         if filter is True:
             await page.locator("button.header").click()
@@ -48,26 +61,39 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
                 has_text="Beneficiário de Programa Social"
             ).click()
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         input_field = page.locator("#termo")
         if await input_field.is_visible() and await input_field.is_enabled():
             await input_field.fill(input_data)
             await asyncio.sleep(1)
             await input_field.press("Enter")
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         await page.wait_for_selector("span#resultados")
+
+        await page.mouse.move(random.random() * 800, random.random() * 800)
 
         search_name_link = page.locator("a.link-busca-nome").nth(0)
         if await search_name_link.is_visible():
+            await page.mouse.move(random.random() * 800, random.random() * 800)
             await search_name_link.click()
         else:
             print(f"no data was found for {input_data}")
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         await page.wait_for_load_state("load")
+
+        await page.mouse.move(random.random() * 800, random.random() * 800)
 
         if await cookie_btn.is_visible():
             await cookie_btn.click()
 
         incomes_button = page.locator("button.header")
+
+        await page.mouse.move(random.random() * 800, random.random() * 800)
 
         await asyncio.sleep(0.3)
 
@@ -76,10 +102,14 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
         else:
             print("button incomes not found")
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         await page.wait_for_load_state()
 
         beneficts = []
         benefict_deatils = []
+
+        await page.mouse.move(random.random() * 800, random.random() * 800)
 
         screenshot_path = "page.png"
         await page.screenshot(path=screenshot_path, full_page=True)
@@ -88,9 +118,15 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
             "section.dados-tabelados > div.row > div"
         ).all()
 
+        await page.mouse.move(random.random() * 800, random.random() * 800)
+
         for person_data_index, person_data in enumerate(person_data_row):
+
+            await page.mouse.move(random.random() * 800, random.random() * 800)
             if person_data_index == len(person_data_row) - 1:
                 break
+
+            await page.mouse.move(random.random() * 800, random.random() * 800)
             key = await person_data.locator("strong").inner_text()
             value = await person_data.locator("span").inner_text()
             results[key.strip().lower()] = value.strip()
@@ -100,6 +136,7 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
         ).count()
 
         for benefict_index in range(all_beneficts):
+            await page.mouse.move(random.random() * 800, random.random() * 800)
             benefict_name = (
                 await page.locator("div.responsive > strong")
                 .nth(benefict_index)
@@ -116,13 +153,19 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
             actual_benefict["nome_do_beneficio"] = benefict_name
             actual_benefict["valor"] = recieved_value.strip()
 
+            await page.mouse.move(random.random() * 800, random.random() * 800)
+
             await page.wait_for_load_state("load")
+
+            await page.mouse.move(random.random() * 800, random.random() * 800)
 
             if benefict_index == 0:
                 await asyncio.sleep(13)
                 await page.wait_for_load_state("load")
             else:
                 await asyncio.sleep(1)
+
+            await page.mouse.move(random.random() * 800, random.random() * 800)
 
             if await cookie_btn.is_visible():
                 await cookie_btn.click()
@@ -131,20 +174,29 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
 
             await asyncio.sleep(1)
 
+            await page.mouse.move(random.random() * 800, random.random() * 800)
+
             recieved_incomes_keys = []
 
             for th in recieved_incomes_thead:
+
                 text = await th.inner_text()
                 recieved_incomes_keys.append(text.strip())
+
+            await page.mouse.move(random.random() * 800, random.random() * 800)
 
             await asyncio.sleep(1)
 
             recieved_incomes_values = await page.locator("tr").all()
 
             for tr in recieved_incomes_values:
+
+                await page.mouse.move(random.random() * 800, random.random() * 800)
+
                 item = await tr.locator("td").all()
                 data = {}
                 for i, td in enumerate(item):
+                    await page.mouse.move(random.random() * 800, random.random() * 800)
                     income_value = await td.inner_text()
                     data[recieved_incomes_keys[i]] = income_value.strip()
                     benefict_deatils.append(data)
@@ -153,6 +205,8 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
             beneficts.append(actual_benefict)
             results["beneficios"] = beneficts
 
+            await page.mouse.move(random.random() * 800, random.random() * 800)
+
             await page.go_back()
             await page.wait_for_load_state("load")
 
@@ -160,6 +214,8 @@ async def collect_data_async(input_data: str, filter: bool = False) -> SearchRes
 
             if await cookie_btn.is_visible():
                 await cookie_btn.click()
+
+            await page.mouse.move(random.random() * 800, random.random() * 800)
 
             await page.locator("button.header").nth(0).click()
 
